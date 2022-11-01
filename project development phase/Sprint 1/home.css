7a3c8a3f5173120aa12955e13a167b83a9ecbc19
abiramir082001 <115152057+abiramir082001@users.noreply.github.com>
2022-11-01 09:56:16 +0530

    Create home.css

---
+++ project development phase/Sprint 1/home.css
@@ -0,0 +1,165 @@
+/* font links */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+body{
+    margin: 0;
+    padding: 0;
+}
+.homeimage
+{
+   height: 500px;
+   width: 500px;
+   align-items: center;
+   display: flex;
+}
+
+/* Navbar  */
+.nav{
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: rgb(247, 247, 247);
+    height: 80px;
+}
+.logo{
+    top: 0;
+    width: 1px;
+    padding-top: 5px;
+    height: auto;
+    padding-right: 0%;
+}
+.center{
+    top:0;
+    width: 100%;
+    left: 0;
+    height: auto;
+    z-index: 100;
+    font-weight:bold;
+    font-family: Georgia, Times, 'Times New Roman', serif;
+    transition: .3s;
+   
+}
+.items > ul{
+    display: flex;
+    align-items: center;
+}
+ul li{
+    list-style-type: none;
+    margin: 0 20px;
+    transition: .3s;
+}
+ul li:hover{
+    text-decoration: underline;
+}
+li > a{
+    text-decoration: none;
+    color: #000;
+    font-family: roboto;
+    font-size: 1em;
+}
+.btn{
+    background: rgb(223, 223, 223);
+    width: auto;
+    height: 30px;
+    padding: 0 10px;
+    color: #000;
+    border: 2px solid transparent;
+    border-radius: 8px;
+    font-weight: 400;
+    font-size: 1em;
+    cursor: pointer;
+    transition: .3s ;
+}
+.btn:hover{
+    background: #000;
+    color: rgb(223, 223, 223);
+}
+/* navbar end */
+
+/* Sign In Form */
+
+.login_sec{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    /* background: #000; */
+}
+.container{
+    width: 500px;
+    height:auto;
+    /* background: rgb(231, 231, 231); */
+    margin-top: 5em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.heading h1{
+    font-size: 30px;
+    font-family:monospace;
+    letter-spacing: 1px;
+    text-align: center;
+}
+.heading .google_id{
+    padding: 5px;
+    background: #4267B2;
+    color: #ffff;
+    border: 2px solid transparent;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: .3s ;
+    letter-spacing: 1px;
+}
+.inp-items{
+    margin: 30px 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.inp-items label{
+    display: flex;
+    flex-direction: column;
+    font-size: 14px;
+    padding: 10px 0;
+}
+label .inp{
+    margin: 10px 0;
+    padding: 7px;
+    border-radius: 7px;
+    border: 1px solid rgb(218, 218, 218);
+    transition: .1s;
+    outline: 1px solid #fff;
+    width: 230px;
+}
+label .inp:focus{
+    outline: 1px solid #406cc4;
+    border: 1px solid #406cc4;
+}
+.inline{
+    display: flex;
+    justify-content: space-between;
+}
+.inline .pass_fgt{
+    color: #406cc4;
+    text-decoration: underline;
+    cursor: pointer;
+}
+#submit_btn{
+    background: rgb(5, 172, 5);
+    color: #fff;
+    padding: 7px;
+    border: 2px solid #fff;
+    border-radius: 8px;
+    font-size: 17px;
+    transition: .2s;
+    cursor: pointer;
+}
+#submit_btn:hover{
+    background: #fff;
+    color: rgb(5, 172, 5);
+    border: 2px solid rgb(5, 172, 5);
+}
+
+/* Sign In Form End */
